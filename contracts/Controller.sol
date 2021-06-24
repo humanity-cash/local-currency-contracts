3ac2ccea2007d7596dae230998ebb22a6f034118
Esraa Jbara <jbesraa@gmail.com>
2021-06-24 10:19:21 +0300

    fix identation

--- contracts/Controller.sol
+++ contracts/Controller.sol
@@ -20,52 +20,52 @@ import "./interface/IVersionedContract.sol";
  * @author Aaron Boyd <https://github.com/aaronmboyd>
  */
 contract Controller is IVersionedContract, Ownable, Pausable, ReentrancyGuard {
-	using SafeMath for uint256;
-	using SafeERC20 for IERC20;
-	using SafeERC20 for ERC20PresetMinterPauser;
-	using EnumerableMap for EnumerableMap.UintToAddressMap;
+		using SafeMath for uint256;
+		using SafeERC20 for IERC20;
+		using SafeERC20 for ERC20PresetMinterPauser;
+		using EnumerableMap for EnumerableMap.UintToAddressMap;
 
-	/**
-	 * @notice Triggered when a new user has been created
-	 *
-	 * @param _userId           Hashed bytes32 of the userId
-	 * @param _walletAddress    Address of the wallet
-	 */
-	event NewUser(bytes32 indexed _userId, address indexed _walletAddress);
+		/**
+		 * @notice Triggered when a new user has been created
+		 *
+		 * @param _userId           Hashed bytes32 of the userId
+		 * @param _walletAddress    Address of the wallet
+		 */
+		event NewUser(bytes32 indexed _userId, address indexed _walletAddress);
 
-	/**
-	 * @notice Triggered when the Wallet Factory is updated
-	 *
-	 * @param _oldFactoryAddress   Old factory address
-	 * @param _newFactoryAddress   New factory address
-	 */
-	event FactoryUpdated(address indexed _oldFactoryAddress, address indexed _newFactoryAddress);
+		/**
+		 * @notice Triggered when the Wallet Factory is updated
+		 *
+		 * @param _oldFactoryAddress   Old factory address
+		 * @param _newFactoryAddress   New factory address
+		 */
+		event FactoryUpdated(address indexed _oldFactoryAddress, address indexed _newFactoryAddress);
 
-	IERC20 public erc20Token;
-	IWalletFactory public walletFactory;
+		IERC20 public erc20Token;
+		IWalletFactory public walletFactory;
 
-	// Mapping of Wallet identifiers to their contract address
-	EnumerableMap.UintToAddressMap private wallets;
+		// Mapping of Wallet identifiers to their contract address
+		EnumerableMap.UintToAddressMap private wallets;
 
-	/**
-	* @notice Used to initialize a new Controller contract
-	*
-	* @param _erc20Token token used
-	 */
-	constructor(
-			address _erc20Token,
-			address _factory
-		   )
-	{
-		erc20Token = IERC20(_erc20Token);
-		walletFactory = IWalletFactory(_factory);
-	}
+		/**
+		 * @notice Used to initialize a new Controller contract
+		 *
+		 * @param _erc20Token token used
+		 */
+		constructor(
+				address _erc20Token,
+				address _factory
+		)
+		{
+				erc20Token = IERC20(_erc20Token);
+				walletFactory = IWalletFactory(_factory);
+		}
 
-	/**
-	 * @notice Returns the storage, major, minor, and patch version of the contract.
-	 * @return The storage, major, minor, and patch version of the contract.
-	 */
-	function getVersionNumber()
+		/**
+		 * @notice Returns the storage, major, minor, and patch version of the contract.
+		 * @return The storage, major, minor, and patch version of the contract.
+		 */
+		function getVersionNumber()
 		external
 		pure
 		override
@@ -74,78 +74,78 @@ contract Controller is IVersionedContract, Ownable, Pausable, ReentrancyGuard {
 				uint256,
 				uint256,
 				uint256
-			)
+		)
 		{
-			return (1, 2, 0, 0);
+				return (1, 2, 0, 0);
 		}
 
-	/**
-	 * @notice Enforces values > 0 only
-	 */
-	modifier greaterThanZero(uint256 _value) {
-		require(_value > 0, "ERR_ZERO_VALUE");
-		_;
-	}
+		/**
+		 * @notice Enforces values > 0 only
+		 */
+		modifier greaterThanZero(uint256 _value) {
+				require(_value > 0, "ERR_ZERO_VALUE");
+				_;
+		}
 
-	/**
-	* @notice Enforces value to not be greater than a user's available balance
-	 */
-	modifier balanceAvailable(bytes32 _userId, uint256 _value) {
-		require(balanceOfWallet(_userId) >= _value, "ERR_NO_BALANCE");
-		_;
-	}
+		/**
+		* @notice Enforces value to not be greater than a user's available balance
+		*/
+		modifier balanceAvailable(bytes32 _userId, uint256 _value) {
+				require(balanceOfWallet(_userId) >= _value, "ERR_NO_BALANCE");
+				_;
+		}
 
-	/**
-	* @notice Enforces a _userId should not be mapped to an existing user / contract address
-	*/
-	modifier userNotExist(bytes32 _userId) {
-		require(!wallets.contains(uint256(_userId)), "ERR_USER_EXISTS");
-		_;
-	}
+		/**
+		* @notice Enforces a _userId should not be mapped to an existing user / contract address
+		*/
+		modifier userNotExist(bytes32 _userId) {
+				require(!wallets.contains(uint256(_userId)), "ERR_USER_EXISTS");
+				_;
+		}
 
-	/**
-	 * @notice Public update to a new Wallet Factory
-	 *
-	 * @param _newFactoryAddress   new factory address
-	 */
-	function setWalletFactory(address _newFactoryAddress) external onlyOwner {
-		_setWalletFactory(_newFactoryAddress);
-	}
+		/**
+		* @notice Public update to a new Wallet Factory
+		*
+				* @param _newFactoryAddress   new factory address
+		*/
+		function setWalletFactory(address _newFactoryAddress) external onlyOwner {
+				_setWalletFactory(_newFactoryAddress);
+		}
 
-	/**
-	 * @notice Internal implementation of update to a new Wallet Factory
-	 *
-	 * @param _newFactoryAddress   new factory address
-	 */
-	function _setWalletFactory(address _newFactoryAddress) private {
-		walletFactory = IWalletFactory(_newFactoryAddress);
-		emit FactoryUpdated(address(walletFactory), _newFactoryAddress);
-	}
+		/**
+		* @notice Internal implementation of update to a new Wallet Factory
+		*
+				* @param _newFactoryAddress   new factory address
+		*/
+		function _setWalletFactory(address _newFactoryAddress) private {
+				walletFactory = IWalletFactory(_newFactoryAddress);
+				emit FactoryUpdated(address(walletFactory), _newFactoryAddress);
+		}
 
-	/**
-	* @notice Retrieves the available balance of a wallet
-	*
-	* @param _userId user identifier
-	* @return uint256 available balance
-	 */
-	function balanceOfWallet(bytes32 _userId) public view returns (uint256) {
-		address walletAddress = wallets.get(uint256(_userId));
-		IWallet wallet = IWallet(walletAddress);
-		return wallet.availableBalance();
-	}
+		/**
+		* @notice Retrieves the available balance of a wallet
+		*
+				* @param _userId user identifier
+		* @return uint256 available balance
+		*/
+		function balanceOfWallet(bytes32 _userId) public view returns (uint256) {
+				address walletAddress = wallets.get(uint256(_userId));
+				IWallet wallet = IWallet(walletAddress);
+				return wallet.availableBalance();
+		}
 
-	/**
-	* @notice Settles an amount for a wallet and transfers to the wallet contract
-	*
-	* @param _userId       User identifier
-	* @param _txId         Raw transaction ID for this event
-	* @param _value        Amount to settle
-	 */
-	function settle(
-			bytes32 _userId,
-			string calldata _txId,
-			uint256 _value
-		       )
+		/**
+		* @notice Settles an amount for a wallet and transfers to the wallet contract
+		*
+				* @param _userId       User identifier
+				* @param _txId         Raw transaction ID for this event
+						* @param _value        Amount to settle
+				*/
+		function settle(
+				bytes32 _userId,
+				string calldata _txId,
+				uint256 _value
+		)
 		external
 		greaterThanZero(_value)
 		balanceAvailable(_userId, _value)
@@ -153,169 +153,169 @@ contract Controller is IVersionedContract, Ownable, Pausable, ReentrancyGuard {
 		nonReentrant
 		whenNotPaused
 		{
-			_settle(uint256(_userId), _txId, _value);
+				_settle(uint256(_userId), _txId, _value);
 		}
 
 		/**
 		* @notice Settles an amount for a wallet and transfers to the wallet contract
 		* @dev Implementation of external "settle" function so that it may be called internally without reentrancy guard incrementing
 		*
-		* @param _userId       User identifier
+				* @param _userId       User identifier
 		* @param _txId         Raw transaction ID for this event
-		* @param _value        Amount to settle
-		 */
-	function _settle(
-			uint256 _userId,
-			string calldata _txId,
-			uint256 _value
-			) private {
-		address tmpWalletAddress = wallets.get(_userId);
-		IWallet user = IWallet(tmpWalletAddress);
-		user.settle(_txId, _value);
-	}
+				* @param _value        Amount to settle
+		*/
+		function _settle(
+				uint256 _userId,
+				string calldata _txId,
+				uint256 _value
+		) private {
+				address tmpWalletAddress = wallets.get(_userId);
+				IWallet user = IWallet(tmpWalletAddress);
+				user.settle(_txId, _value);
+		}
 
 		/**
 		* @notice create a new user and assign them a wallet contract
 		*
-		* @param _userId user identifier
-		 */
-	function newWallet(string calldata _userId)
+				* @param _userId user identifier
+		*/
+		function newWallet(string calldata _userId)
 		external
 		onlyOwner
 		nonReentrant
 		whenNotPaused
 		userNotExist(keccak256(bytes(_userId)))
 		{
-			address newWalletAddress = walletFactory.createProxiedWallet(_userId);
-			bytes32 key = keccak256(bytes(_userId));
-			wallets.set(uint256(key), newWalletAddress);
+				address newWalletAddress = walletFactory.createProxiedWallet(_userId);
+				bytes32 key = keccak256(bytes(_userId));
+				wallets.set(uint256(key), newWalletAddress);
 
-		emit NewUser(key, newWalletAddress);
-    }
+				emit NewUser(key, newWalletAddress);
+		}
 
-    /**
-     * @notice retrieve contract address for a Wallet
-     *
-     * @param _userId user identifier
-     * @return address of user's contract
-     */
+		/**
+		* @notice retrieve contract address for a Wallet
+		*
+		* @param _userId user identifier
+		* @return address of user's contract
+		 */
 
-    function getWalletAddress(bytes32 _userId) public view returns (address) {
-	    return wallets.get(uint256(_userId), "ERR_USER_NOT_EXIST");
-    }
+		function getWalletAddress(bytes32 _userId) public view returns (address) {
+				return wallets.get(uint256(_userId), "ERR_USER_NOT_EXIST");
+		}
 
-    function transferContractOwnership(address newOwner) public onlyOwner {
-	    super.transferOwnership(newOwner);
-    }
+		function transferContractOwnership(address newOwner) public onlyOwner {
+				super.transferOwnership(newOwner);
+		}
 
-    function transferWalletOwnership(address newOwner, bytes32 uid) public  onlyOwner {
-			address walletAddress = wallets.get(uint256(uid));
-	    IWallet user = IWallet(walletAddress);
-	    user.transferController(newOwner);
-    }
+		function transferWalletOwnership(address newOwner, bytes32 uid) public  onlyOwner {
+				address walletAddress = wallets.get(uint256(uid));
+				IWallet user = IWallet(walletAddress);
+				user.transferController(newOwner);
+		}
 
-    // /**
-    // * @notice Transfers ownership of the contract to a new account (`newOwner`).
-    // * Can only be called by the current owner.
-    // *
-    // * @dev In this override, we iterate all the existing Wallet contracts
-    // * and change their owner before changing the owner of the core contract
-    // *
-    // * @param newOwner new owner of this contract
-    // * @inheritdoc Ownable
-    // *
-    //  */
+		// /**
+		// * @notice Transfers ownership of the contract to a new account (`newOwner`).
+		// * Can only be called by the current owner.
+		// *
+		// * @dev In this override, we iterate all the existing Wallet contracts
+		// * and change their owner before changing the owner of the core contract
+		// *
+		// * @param newOwner new owner of this contract
+		// * @inheritdoc Ownable
+		// *
+		//  */
 
-    function getReal() public view returns (address) {
-		    address walletAddress;
-		    (, walletAddress) = wallets.at(0);
+		function getReal() public view returns (address) {
+				address walletAddress;
+				(, walletAddress) = wallets.at(0);
 				return walletAddress;
-	  }
+		}
 
-    function transferOwnership(address newOwner) public override onlyOwner {
-	    // 1 Update owner on all Wallet contracts
-	    uint256 i;
-			address result;
-	    for (i = 0; i < wallets.length(); i = i.add(1)) {
-		    address walletAddress;
+		function transferOwnership(address newOwner) public override onlyOwner {
+				// 1 Update owner on all Wallet contracts
+				uint256 i;
+				address result;
+				for (i = 0; i < wallets.length(); i = i.add(1)) {
+						address walletAddress;
 
-		    // .at function returns a tuple of (uint256, address)
-		    (, walletAddress) = wallets.at(i);
+						// .at function returns a tuple of (uint256, address)
+						(, walletAddress) = wallets.at(i);
 
-		    IWallet user = IWallet(walletAddress);
+						IWallet user = IWallet(walletAddress);
 
-		    user.transferController(newOwner);
-				result = walletAddress;
-	    }
+						user.transferController(newOwner);
+						result = walletAddress;
+				}
 
-	    // 2 Update owner of this contract
-	    super.transferOwnership(newOwner);
-    }
+				// 2 Update owner of this contract
+				super.transferOwnership(newOwner);
+		}
 
-    /**
-     * @notice Update implementation address for beneficiaries
-     *
-     * @param _newLogic New implementation logic for wallet proxies
-     *
-     */
-    function updateWalletImplementation(address _newLogic) external onlyOwner {
-	    uint256 i;
-	    for (i = 0; i < wallets.length(); i = i.add(1)) {
-		    address walletAddress;
-		    // .at function returns a tuple of (uint256, address)
-		    (, walletAddress) = wallets.at(i);
-		    walletFactory.updateProxyImplementation(walletAddress, _newLogic);
-	    }
-    }
+		/**
+		* @notice Update implementation address for beneficiaries
+		*
+				* @param _newLogic New implementation logic for wallet proxies
+						*
+						*/
+		function updateWalletImplementation(address _newLogic) external onlyOwner {
+				uint256 i;
+				for (i = 0; i < wallets.length(); i = i.add(1)) {
+						address walletAddress;
+						// .at function returns a tuple of (uint256, address)
+						(, walletAddress) = wallets.at(i);
+						walletFactory.updateProxyImplementation(walletAddress, _newLogic);
+				}
+		}
 
-    /**
-    * @notice Triggers stopped state.
-    *
-	    * @dev Requirements: The contract must not be paused.
-	    */
-    function pause() external onlyOwner nonReentrant {
-	    _pause();
-    }
+		/**
+		* @notice Triggers stopped state.
+		*
+				* @dev Requirements: The contract must not be paused.
+				*/
+		function pause() external onlyOwner nonReentrant {
+				_pause();
+		}
 
-    /**
-    * @notice Returns to normal state.
-    *
-	    * @dev Requirements: The contract must be paused.
-	    */
-    function unpause() external onlyOwner nonReentrant {
-	    _unpause();
-    }
+		/**
+		* @notice Returns to normal state.
+		*
+				* @dev Requirements: The contract must be paused.
+				*/
+		function unpause() external onlyOwner nonReentrant {
+				_unpause();
+		}
 
 
-    /**
-    * @notice Emergency withdrawal of all remaining token to the owner account
-    *
-	    * @dev The contract must be paused
-    * @dev Sends erc20 to current owner
-    */
-    function withdrawToOwner() external onlyOwner whenPaused nonReentrant {
-	    uint256 balanceOf = erc20Token.balanceOf(address(this));
-	    erc20Token.transfer(owner(), balanceOf);
-    }
+		/**
+		* @notice Emergency withdrawal of all remaining token to the owner account
+		*
+				* @dev The contract must be paused
+		* @dev Sends erc20 to current owner
+		*/
+		function withdrawToOwner() external onlyOwner whenPaused nonReentrant {
+				uint256 balanceOf = erc20Token.balanceOf(address(this));
+				erc20Token.transfer(owner(), balanceOf);
+		}
 
-    /**
-    * @notice Get wallet address at index
-    * @dev Used for iterating the complete list of beneficiaries
-	    *
-	    */
-    function getWalletAddressAtIndex(uint256 _index) external view returns (address) {
-	    // .at function returns a tuple of (uint256, address)
-	    address walletAddress;
-	    (, walletAddress) = wallets.at(_index);
+		/**
+		* @notice Get wallet address at index
+		* @dev Used for iterating the complete list of beneficiaries
+				*
+				*/
+		function getWalletAddressAtIndex(uint256 _index) external view returns (address) {
+				// .at function returns a tuple of (uint256, address)
+				address walletAddress;
+				(, walletAddress) = wallets.at(_index);
 
-	    return walletAddress;
-    }
+				return walletAddress;
+		}
 
-    /**
-    * @notice Get count of beneficiaries
-    *
-	    */
-    function getWalletCount() external view returns (uint256) {
-	    return wallets.length();
-    }
+		/**
+		* @notice Get count of beneficiaries
+		*
+				*/
+		function getWalletCount() external view returns (uint256) {
+				return wallets.length();
+		}
 }
--- test/Controller.test.js
+++ test/Controller.test.js
@@ -7,102 +7,102 @@ const Web3Utils = require('web3-utils');
 const { uuid } = require("uuidv4");
 
 function toBytes32(input) {
-  return Web3Utils.keccak256(input);
+		return Web3Utils.keccak256(input);
 }
 
 contract.only("Controller", async (accounts) => {
-	const owner = accounts[0];
+		const owner = accounts[0];
 
-	let controller, factory, testToken, wallet;
+		let controller, factory, testToken, wallet;
 
-	before(async () => {
-		let deployment = await deploy();
+		before(async () => {
+				let deployment = await deploy();
 
-		wallet = deployment.wallet;
-		controller = deployment.controller;
-		factory = deployment.factory;
-		testToken = deployment.testToken;
-	});
+				wallet = deployment.wallet;
+				controller = deployment.controller;
+				factory = deployment.factory;
+				testToken = deployment.testToken;
+		});
 
-	it("Should read public attributes for important internal addresses", async () => {
-		const token = await controller.erc20Token();
-		assert(token);
-	});
+		it("Should read public attributes for important internal addresses", async () => {
+				const token = await controller.erc20Token();
+				assert(token);
+		});
 
-	it("Should have an owner matching the deployer address", async () => {
-		const contractOwner = await controller.owner();
-		assert.equal(owner, contractOwner, `Owner should be ${owner}`);
-	});
+		it("Should have an owner matching the deployer address", async () => {
+				const contractOwner = await controller.owner();
+				assert.equal(owner, contractOwner, `Owner should be ${owner}`);
+		});
 
-	it("Should be able to update factory", async () => {
-		const factory = await controller.walletFactory();
-		await controller.setWalletFactory(factory);
-	});
+		it("Should be able to update factory", async () => {
+				const factory = await controller.walletFactory();
+				await controller.setWalletFactory(factory);
+		});
 
-	it("Should be able to pause", async () => {
-		await controller.pause();
-	});
+		it("Should be able to pause", async () => {
+				await controller.pause();
+		});
 
-	it("Should be able to unpause", async () => {
-		await controller.unpause();
-	});
+		it("Should be able to unpause", async () => {
+				await controller.unpause();
+		});
 
-	it("Should be able to update wallet proxy logic contract after creating 3 new accounts", async () => {
-		const newLogic = await Wallet.new();
-		await controller.newWallet(uuid());
-		await controller.newWallet(uuid());
-		await controller.newWallet(uuid());
-		await controller.updateWalletImplementation(newLogic.address);
-	});
+		it("Should be able to update wallet proxy logic contract after creating 3 new accounts", async () => {
+				const newLogic = await Wallet.new();
+				await controller.newWallet(uuid());
+				await controller.newWallet(uuid());
+				await controller.newWallet(uuid());
+				await controller.updateWalletImplementation(newLogic.address);
+		});
 
-	it("Should be able to update the factory and create a new user", async () => {
-		const newFactory = await WalletFactory.new(
-			wallet.address,
-			testToken.address
-		);
-		await controller.setWalletFactory(newFactory.address);
-		await controller.newWallet(uuid());
-	});
+		it("Should be able to update the factory and create a new user", async () => {
+				const newFactory = await WalletFactory.new(
+						wallet.address,
+						testToken.address
+				);
+				await controller.setWalletFactory(newFactory.address);
+				await controller.newWallet(uuid());
+		});
 
-	it("Should be able to emergency withdraw after pausing", async () => {
-		await controller.pause();
-		await controller.withdrawToOwner();
-		await controller.unpause();
+		it("Should be able to emergency withdraw after pausing", async () => {
+				await controller.pause();
+				await controller.withdrawToOwner();
+				await controller.unpause();
 
-		const controllerBalanceOfNew = await testToken.balanceOf(
-			controller.address
-		);
+				const controllerBalanceOfNew = await testToken.balanceOf(
+						controller.address
+				);
 
-		assert.equal(
-			controllerBalanceOfNew,
-			0,
-			`Walllet token balance should be ${0}`
-		);
-	});
+				assert.equal(
+						controllerBalanceOfNew,
+						0,
+						`Walllet token balance should be ${0}`
+				);
+		});
 
-	it.only("Should be able to transfer ownership", async () => {
-		// Re-mint some test token to continue testing
-		await testToken.mint(
-			controller.address,
-			Web3.utils.toWei("10000000", "ether")
-		);
-		let user1 = uuid();
-		let user2 = uuid();
-		let user3 = uuid();
+		it.only("Should be able to transfer ownership", async () => {
+				// Re-mint some test token to continue testing
+				await testToken.mint(
+						controller.address,
+						Web3.utils.toWei("10000000", "ether")
+				);
+				let user1 = uuid();
+				let user2 = uuid();
+				let user3 = uuid();
 
-		await controller.newWallet(user1);
-		await controller.newWallet(user2);
-		await controller.newWallet(user3);
-		await controller.transferWalletOwnership(accounts[1], toBytes32(user3));
-		await controller.transferWalletOwnership(accounts[1], toBytes32(user2));
-		await controller.transferWalletOwnership(accounts[1], toBytes32(user1));
+				await controller.newWallet(user1);
+				await controller.newWallet(user2);
+				await controller.newWallet(user3);
+				await controller.transferWalletOwnership(accounts[1], toBytes32(user3));
+				await controller.transferWalletOwnership(accounts[1], toBytes32(user2));
+				await controller.transferWalletOwnership(accounts[1], toBytes32(user1));
 
-		await controller.transferContractOwnership(accounts[1]);
-		const contractOwner = await controller.owner();
-		assert.equal(
-			accounts[1],
-			contractOwner,
-			`Owner should now be ${accounts[1]}`
-		);
-	});
+				await controller.transferContractOwnership(accounts[1]);
+				const contractOwner = await controller.owner();
+				assert.equal(
+						accounts[1],
+						contractOwner,
+						`Owner should now be ${accounts[1]}`
+				);
+		});
 });
